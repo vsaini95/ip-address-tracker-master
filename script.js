1f02d638c634ee6774f41227a0e9a8477324c111
Vanshiiks <90824528+vsaini95@users.noreply.github.com>
2023-08-14 01:21:40 +0530

    Update

--- script.js
+++ script.js
@@ -25,3 +25,40 @@ function onMapClick(e) {
 }
 
 map.on("click", onMapClick);
+
+const detailsdiv = document.querySelector(".details");
+
+const ipHandler = function (ip) {
+  fetch(
+    `https://geo.ipify.org/api/v2/country?apiKey=at_MhI9eO2ZfK6xSWGY7ozkt0qY4sPSk&ipAddress=${ip}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      detailsdiv.innerHTML = `<div class="ip">
+      <p class="details-p">IP ADDRESS</p>
+      <h1>${data.ip}</h1>
+    </div>
+    <div class="location">
+      <p class="details-p">LOCATION</p>
+      <h1>${data.location.country}, ${data.location.region}</h1>
+    </div>
+    <div class="timez">
+      <p class="details-p">TIMEZONE</p>
+      <h1>UTC ${data.location.timezone}</h1>
+    </div>
+    <div class="isp">
+      <p class="details-p">ISP</p>
+      <h1>${data.isp}</h1>
+    </div>`;
+    });
+  inputip.value = "";
+};
+
+const btn = document.querySelector(".form_btn");
+const inputip = document.querySelector(".form-input");
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  let ip = inputip.value;
+  ipHandler(ip);
+});
